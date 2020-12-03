8dc09267d23cb53fad872c75dcff50ac364b2ceb
Serafin <serafinjuarez@yahoo.com>
2020-12-02 21:20:25 -0800

    adding sql file

--- streaming_service.sql
+++ streaming_service.sql
@@ -1,10 +1,23 @@
 CREATE TABLE netflix (
-  Title TEXT PRIMARY KEY,
+  Title VARCHAR PRIMARY KEY,
   Age VARCHAR (10),
 	Streaming_Service TEXT
 );
 CREATE TABLE amazon_prime_video (
-  Title TEXT PRIMARY KEY,
+  Title VARCHAR PRIMARY KEY,
   Age VARCHAR (10),
   Streaming_Service TEXT
 );
+
+
+select * from netflix;
+
+select * from amazon_prime_video;
+
+select * from netflix n join amazon_prime_video apv on n.title = apv.title;
+
+
+select * from netflix n
+FULL OUTER JOIN amazon_prime_video apv
+on n.title = apv.title;
+
