20c4b3488681c21c9af5c9c024ae608e67573a81
Serafin <serafinjuarez@yahoo.com>
2020-12-05 11:37:32 -0800

    adding schema and cleaning jupyter notebook

--- streaming_service.sql
+++ streaming_service.sql
@@ -28,5 +28,35 @@ select * from netflix n
 FULL OUTER JOIN amazon_prime_video apv
 on n.title = apv.title;
 
+--Another attempt at join
+SELECT netflix.title, netflix.age, netflix.streaming_service, amazon_prime_video.title, amazon_prime_video.age, amazon_prime_video.streaming_service
+FROM netflix
+INNER JOIN amazon_prime_video ON netflix.title=amazon_prime_video.title;
+
+
+
+--Counting individual age from Netflix Table
+select age, count(age) from netflix
+group by age
+
+
+--Counting individual age from Prime Table
+select age, count(age) from amazon_prime_video
+group by age
+
+--cleaned up count Netfix
+select streaming_service, age, count(age) as total from netflix
+group by streaming_service, age
+order by total
+
+--cleaned up count Amazon Prime
+select streaming_service, age, count(age) as total from amazon_prime_video
+group by streaming_service, age
+order by total
+
+
+
+
+
 
 
