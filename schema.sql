513fca5cfcf67c626b11f97abe0c26b50b151709
Serafin <serafinjuarez@yahoo.com>
2020-12-05 10:37:02 -0800

    adding schema and cleaning jupyter notebook

---
+++ schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE netflix (
+  Title VARCHAR PRIMARY KEY,
+  Age VARCHAR (10),
+	Streaming_Service TEXT
+);
+CREATE TABLE amazon_prime_video (
+  Title VARCHAR PRIMARY KEY,
+  Age VARCHAR (10),
+  Streaming_Service TEXT
+);
--- streaming_service.sql
+++ streaming_service.sql
@@ -1,23 +1,32 @@
+--Creating Netflix table
 CREATE TABLE netflix (
   Title VARCHAR PRIMARY KEY,
   Age VARCHAR (10),
 	Streaming_Service TEXT
 );
+
+--Creating Prime table
 CREATE TABLE amazon_prime_video (
   Title VARCHAR PRIMARY KEY,
   Age VARCHAR (10),
   Streaming_Service TEXT
 );
 
-
+--testing netflix table
 select * from netflix;
 
+-- testing prime table
 select * from amazon_prime_video;
 
-select * from netflix n join amazon_prime_video apv on n.title = apv.title;
+--Inner join
+select * from netflix n 
+join amazon_prime_video apv 
+on n.title = apv.title;
 
-
+--Full outer join
 select * from netflix n
 FULL OUTER JOIN amazon_prime_video apv
 on n.title = apv.title;
+
+
 
